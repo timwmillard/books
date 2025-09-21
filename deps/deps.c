faa8380f29122cf8f9aac4bee4837ed5cc9be26b
Tim Millard <timwmillard@gmail.com>
2025-09-21 23:22:16 +1000

    cimgui working

--- deps/deps.c
+++ deps/deps.c
@@ -5,11 +5,3 @@
 #include "sokol_glue.h"
 #include "sokol_log.h"
 
-// #include "cimgui/imgui/imgui.h"
-// #define SOKOL_IMGUI_IMPL
-
-// #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
-//
-// #include "imgui/imgui.h"
-// #include "cimgui.h"
-// #include "sokol_imgui.h"
--- deps/deps.cpp
+++ deps/deps.cpp
@@ -1,13 +1,9 @@
 // C++ dependencies file for ImGui integration
-// #include "sokol_app.h"
-// #include "sokol_gfx.h"
-// #include "cimgui/imgui/imgui.h"
-// #define SOKOL_IMGUI_IMPL
-// #include "sokol_imgui.h"
-// 
-//
-#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "sokol_app.h"
+#include "sokol_gfx.h"
 
 #include "imgui/imgui.h"
-#include "cimgui.h"
+
+#define SOKOL_IMGUI_IMPL
 #include "sokol_imgui.h"
+
